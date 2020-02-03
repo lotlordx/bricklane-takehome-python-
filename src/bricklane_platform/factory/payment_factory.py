b6f7a00444e83761b314e4a89ea2c495dbec7f6f
lotlord <infozvil@gmail.com>
2020-02-03 13:17:22 +0100

    Implemented a Payment factory, that knows how to class all instances/class that implements its abstract class

---
+++ src/bricklane_platform/factory/payment_factory.py
@@ -0,0 +1,23 @@
+from inspect import getmembers, isclass, isabstract
+from bricklane_platform.models import payments
+
+
+class PaymentFactory(object):
+	payment_mode = {}
+
+	def __init__(self):
+		self.load_autos()
+
+	def load_autos(self):
+		classes = getmembers(payments, lambda m: isclass(m) and not isabstract(m))
+
+		for name, _type in classes:
+			if isclass(_type) and issubclass(_type, payments.Payment):
+				class_item_dict = {name: _type}
+				self.payment_mode.update(class_item_dict)
+
+	def create_instance(self, payment_type):
+		payment_type = payment_type.title() + 'Payment'
+		if payment_type in self.payment_mode:
+			return self.payment_mode[payment_type]
+		raise Exception("No payment handler for {}".format(payment_type))
