c99870d5fa61668ef2e78827fb9814d2cfc3847b
lotlord <infozvil@gmail.com>
2020-02-03 13:18:52 +0100

    modified the payment processor to read and load data in the dynamic payment classes

--- src/bricklane_platform/services/payment_processor.py
+++ src/bricklane_platform/services/payment_processor.py
@@ -2,15 +2,19 @@ import csv
 
 from bricklane_platform.models.payment import Payment
 
+from bricklane_platform.factory.payment_factory import PaymentFactory
+
 
 class PaymentProcessor(object):
 
     def get_payments(self, csv_path, source):
         payments = []
+        payment_handler = PaymentFactory()
         with open(csv_path) as f:
             reader = csv.DictReader(f)
             for row in reader:
-                payments.append(Payment(row))
+                payment_handler_class = payment_handler.create_instance(source)
+                payments.append(payment_handler_class(row))
 
         return payments
 
