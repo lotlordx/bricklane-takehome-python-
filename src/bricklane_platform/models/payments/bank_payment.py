4bd06023b2c71a3ecbdc8337788c3ec100d44f2d
lotlord <infozvil@gmail.com>
2020-02-03 13:13:57 +0100

    implemented the Bank Payment Concreate Class

---
+++ src/bricklane_platform/models/payments/bank_payment.py
@@ -0,0 +1,18 @@
+from bricklane_platform.models.payments.abs_payment import Payment
+from bricklane_platform.models.bank import Bank
+
+
+class BankPayment(Payment):
+    name = "bank"
+    bank_account_id = None
+
+    def __init__(self, data=None):
+        if not data:
+            return
+        super(BankPayment, self).__init__(data)
+        bank = Bank()
+        bank.bank_account_id = int(data.get("bank_account_id", None))
+        self.bank = bank
+
+    def is_successful(self):
+        return True
