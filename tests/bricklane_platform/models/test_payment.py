736aacab07e53f9d8023d5627022974082b7bd15
lotlord <infozvil@gmail.com>
2020-02-03 13:09:06 +0100

    created Testcase for Bank Payment... Now to the Actual implentation using Factory pattern

--- tests/bricklane_platform/models/test_payment.py
+++ tests/bricklane_platform/models/test_payment.py
@@ -1,64 +1,35 @@
 import unittest
 from datetime import datetime
 
-from bricklane_platform.models.payment import Payment
+from bricklane_platform.models.self.payment import Payment
 from bricklane_platform.models.card import Card
 
 
-class TestPayment(unittest.TestCase):
+class TestBankPayment(unittest.TestCase):
 
-    def test_init(self):
-        payment = Payment()
+    def setUp(self):
+        self.payment = BankPayment()
 
-        self.assertIsNone(payment.customer_id)
-        self.assertIsNone(payment.date)
-        self.assertIsNone(payment.amount)
-        self.assertIsNone(payment.fee)
-        self.assertIsNone(payment.card_id)
+    def test_set_up(self):
 
-    def test_init_with_data(self):
 
-        data = {
-            "amount": "2000",
-            "card_id": "45",
-            "card_status": "processed",
-            "customer_id": "123",
-            "date": "2019-02-01",
+        self.assertIsNone(self.payment.customer_id)
+        self.assertIsNone(self.payment.date)
+        self.assertIsNone(self.payment.amount)
+        self.assertIsNone(self.payment.fee)
+        self.assertIsNone(self.payment.bank_account_id)
+
+    def test_create_bank_payment(self):
+
+        test_data = {
+            "customer_id": "5340",
+            "amount": "4433",
+            "date": "2001-06-11",
+            "bank_account_id": "1000"
         }
 
-        payment = Payment(data)
-
-        self.assertEqual(payment.customer_id, 123)
-        self.assertEqual(payment.date, datetime(2019, 2, 1))
-        self.assertEqual(payment.amount, 1960)
-        self.assertEqual(payment.fee, 40)
-
-        card = payment.card
-
-        self.assertIsInstance(card, Card)
-        self.assertEqual(card.card_id, 45)
-        self.assertEqual(card.status, "processed")
-
-    def test_is_successful(self):
-        card = Card()
-        card.status = "processed"
-        payment = Payment()
-        payment.card = card
-
-        self.assertTrue(payment.is_successful())
-
-    def test_is_successful_declined(self):
-        card = Card()
-        card.status = "declined"
-        payment = Payment()
-        payment.card = card
-
-        self.assertFalse(payment.is_successful())
-
-    def test_is_successful_errored(self):
-        card = Card()
-        card.status = "errored"
-        payment = Payment()
-        payment.card = card
-
-        self.assertFalse(payment.is_successful())
+        self.payment = BankPayment(test_data)
+        self.assertEqual(self.payment.amount, 4433)
+        self.assertEqual(self.payment.customer_id, 5340)
+        self.assertEqual(self.payment.fee, 2
+        self.assertEqual(self.payment.date, datetime(2001, 06, 11))
